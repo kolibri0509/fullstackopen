7f0af4b0b9821f94dfce5ac41befba2e0983c873
Mariya Spiridonova <kolibri0509@gmail.com>
2023-11-21 10:40:33 +0300

    step1

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -1,25 +1,34 @@
+const Header = (props) => {
+  return(
+    <h1>{props.course}</h1>
+  ) 
+}
+
+const Content = (props) => {
+  return(
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>  
+  )
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of exercises {props.exercises}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const part = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
+  const exercises = [10, 7, 14]
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part={part[0]} exercises={exercises[0]}/>
+      <Content part={part[1]} exercises={exercises[1]}/>
+      <Content part={part[2]} exercises={exercises[2]}/>
+      <Total exercises={exercises.reduce(function(a, b){return a+b},0)}/>
     </div>
   )
 }
