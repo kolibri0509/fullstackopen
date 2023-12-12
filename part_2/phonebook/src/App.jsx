3861c5b544332445805f5e72571dedb0379e30ee
Mariya Spiridonova <kolibri0509@gmail.com>
2023-12-12 11:52:09 +0300

    step4

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -2,14 +2,28 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number : '040-1234567',
-      id:1 
-    }
+    { name: 'Arto Hellas',number : '040-1234567', id:1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
+
+  // name search
+  const [findNameValue, setFindNameValue] = useState('')
+
+  const findName = (event) => {
+    setFindNameValue(event.target.value)  
+  } 
+  const filterName= persons.filter(person=>person.name.toLowerCase().includes(findNameValue.toLowerCase()))
+  
+  const showNames = findNameValue 
+  ? filterName.map(person=><p key={person.id}>{person.name} {person.number}</p>)
+  : persons.map(person=><p key={person.id}>{person.name} {person.number}</p>)
+
+  // adding name and number
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  
   const writeName = (event) => {
     setNewName(event.target.value)
   }
@@ -33,18 +47,22 @@ const App = () => {
       }else if(newNumber === person.number){
           alert(`${newNumber} is already added to phonebook`)
           return setNewNumber(''),setNewName(''),setPersons(persons)
-        }
+      }
       else{
         return setNewName(''),setNewNumber(''),
         setPersons(persons.concat(nameObject))
         }
       })             
-    }  
+    } 
   }        
-     
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={findName} value={findNameValue}/>
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addNewName}>
         <div>
           name: <input onChange={writeName} value={newName}/> <br/> <br/>
@@ -55,7 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map(person=><p key={person.id}>{person.name} {person.number}</p>)}</div>
+      <div>{showNames}</div>
     </div>
   )
 }
