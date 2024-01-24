69aa516492f35cb9fc0be0642d792f8c0977eaa3
Mariya Spiridonova <kolibri0509@gmail.com>
2024-01-24 15:56:55 +0300

    mongoDb_Atlass

--- part_3/notes/index.js
+++ part_3/notes/index.js
@@ -13,15 +13,11 @@ const requestLogger = (request, response, next) => {
   console.log('---');
   next();
 }
-// Промежуточное программное обеспечение для перехвата запросов к несуществующим маршрутам
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
 
-app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(requestLogger);
-app.use(express.static('dist'));
+app.use(cors());
 
 let notes = []
 
@@ -30,15 +26,21 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
   })
   })
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note)
+    if(note){
+      response.json(note)
+    }else{
+      response.status(404).end()
+    }  
   })
+  .catch( error => next(error))
 })
 app.delete('/api/notes/:id', (request,response)=> {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id != id)
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
+    .then(result=>
+      response.status(204).end())
+    .catch(error=> next(error)) 
 })
 
 app.post('/api/notes', (request, response) => {
@@ -57,7 +59,35 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+// Промежуточное программное обеспечение для перехвата запросов к несуществующим маршрутам
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 app.use(unknownEndpoint)
+
+// Обработчик ошибок
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT 
 app.listen(PORT,(err) =>{
