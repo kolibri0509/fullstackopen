62fdd2b189d645670fe70ea340e51b8a5bd1c1f0
Mariya Spiridonova <kolibri0509@gmail.com>
2024-01-29 14:12:21 +0300

    note_validation

--- part_3/notes/index.js
+++ part_3/notes/index.js
@@ -13,13 +13,25 @@ const requestLogger = (request, response, next) => {
   console.log('---');
   next();
 }
+// Промежуточное программное обеспечение для перехвата запросов к несуществующим маршрутам
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+// Обработчик ошибок
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }else if(error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
 
 app.use(express.static('dist'));
 app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
-
-let notes = []
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
@@ -43,50 +55,36 @@ app.delete('/api/notes/:id', (request,response)=> {
     .catch(error=> next(error)) 
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-  if(!body.content){
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
+  
   const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
+    important: body.important || false,
+    date: new Date(),
   })
-  note.save().then(savedNote => {
+  note.save()
+  .then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  const { content, important } = request.body
+  
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+    )
     .then(updatedNote => {
       response.json(updatedNote)
     })
     .catch(error => next(error))
 })
-// Промежуточное программное обеспечение для перехвата запросов к несуществующим маршрутам
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
 
-// Обработчик ошибок
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
-  next(error)
-}
+app.use(unknownEndpoint)
 app.use(errorHandler)
 
 const PORT = process.env.PORT 
