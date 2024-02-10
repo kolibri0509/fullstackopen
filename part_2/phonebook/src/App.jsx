a9acc2d22e26a48106ca95edb8c6737ff3b1f298
Mariya Spiridonova <kolibri0509@gmail.com>
2024-02-10 13:42:24 +0300

    update setError

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -143,13 +143,14 @@ const App = () => {
           },5000)
         })
         .catch(error => {
-          if(newName.length < 3){
-            setError(`Path 'name' (${newName}) is shorter than the minimum allowed length (3)`)
-          }else if(newNumber.length < 8){
-            setError(`Path 'number' (${newNumber}) is shorter than the minimum allowed length (8)`)
-          }else{
-            setError('Valid number format is XX-... or XXX-...')
-          }          
+          // if(newName.length < 3){
+          //   setError(`Path 'name' (${newName}) is shorter than the minimum allowed length (3)`)
+          // }else if(newNumber.length < 8){
+          //   setError(`Path 'number' (${newNumber}) is shorter than the minimum allowed length (8)`)
+          // }else{
+          //   setError('Valid number format is XX-... or XXX-...')
+          // }         
+          setError(error.response.data.error) 
           setTimeout(()=>{
             setError(null)
           },5000)
